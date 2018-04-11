fdf29b5141d0756bb4a585eea36a3bc132373dbe
Allen Kerr <allen.kerr@airbnb.com>
2018-04-11 10:36:20 -0700

    Add ESlint and cleanup javascript

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+function showCopyAlert() {
+  $('#copy-alert').show();
+  setTimeout(() => {
+    $('#copy-alert').hide();
+  }, 2000);
+}
+
 function copySnippet() {
   /* Get the text field */
-  $("#code-snippet").select();
+  $('#code-snippet').select();
 
   /* Copy the text inside the text field */
-  document.execCommand("Copy");
+  document.execCommand('Copy');
 
   /* Alert the copied text */
   showCopyAlert();
-}
-
-function showCopyAlert(){
-  $("#copy-alert").show();
-  setTimeout(function(){
-    $("#copy-alert").hide();
-  }, 2000);
 }
 
 $('#copy-button').on('click', copySnippet);
--- rummager.js
+++ rummager.js
@@ -1,5 +1,69 @@
-function overlayRummager() {
-  if ($('#notesFilter').length === 0 && $('#itemFilter').length !== 0) {
+class Rummager {
+  static matchesFilter(filterTextArray, value) {
+    return filterTextArray.reduce((isMatched, filter) => {
+      if (!isMatched) { return false; }
+      return filter.startsWith('^') ? !value.includes(filter.substr(1)) : value.includes(filter);
+    }, true);
+  }
+
+  static clearFilters() {
+    $('#itemFilter').val('');
+    $('#notesFilter').val('');
+    $('#countryFilter').val('');
+    $('#priceFilter').val('');
+    $('#itemFilter').keyup();
+  }
+
+  static generateFilterArray(item) {
+    return item.val().toLowerCase().trim().split(' ');
+  }
+
+  static handleFilter() {
+    $('.item').hide();
+
+    const onlyAvailable = $('#showAvailableOnly').is(':checked');
+    const onlyUnsigned = $('#showUnsignedOnly').is(':checked');
+    let $rows = (onlyAvailable) ? $('.item:not(.historic-item)') : $('.item');
+
+    if (onlyUnsigned) {
+      $rows =
+        $($rows.toArray().filter(item => $(item).find('.request-status').text().trim() === 'REQ'));
+    }
+
+
+    const itemFilterTextArray = Rummager.generateFilterArray($('#itemFilter'));
+    const notesFilterTextArray = Rummager.generateFilterArray($('#notesFilter'));
+    const countryFilterTextArray = Rummager.generateFilterArray($('#countryFilter'));
+    const priceFilter = $('#priceFilter').val();
+
+    $rows.each((index, row) => {
+      const $row = $(row);
+      const item = {
+        name: $row.find('.item-name').text().toLowerCase(),
+        notes: $row.find('.notes').text().toLowerCase(),
+        price: Number($($row.children()[2]).text().replace(/[^0-9.]+/g, '')),
+        country: $($row.children()[0]).text().toLowerCase(),
+      };
+      const rowMatched =
+        Rummager.matchesFilter(itemFilterTextArray, item.name) &&
+        Rummager.matchesFilter(notesFilterTextArray, item.notes) &&
+        Rummager.matchesFilter(countryFilterTextArray, item.country) &&
+        (priceFilter === '' || item.price <= priceFilter);
+
+      if (rowMatched) {
+        $row.show();
+      } else {
+        $row.hide();
+      }
+    });
+
+    $('#count').html($('.item:visible').length);
+  }
+
+  static overlayRummager() {
+    if ($('#notesFilter').length !== 0 || $('#itemFilter').length === 0) {
+      return;
+    }
     $('#itemFilter').attr({ placeholder: 'Search Name' });
     $('<input type="number" class="form-control" id="priceFilter" placeholder="Upper Price Limit">')
       .insertAfter($('#itemFilter'));
@@ -9,72 +73,16 @@ function overlayRummager() {
       .insertAfter($('#itemFilter'));
     $('<input type="checkbox" id="showUnsignedOnly">Hide Signed & Requested<br/>')
       .insertAfter($('.item-locator'));
-    $('<br/><button class="btn btn-primary" id="clearAll">Clear Filters</button>')
+    $('<br/><button class="btn btn-primary" id="clearFilters">Clear Filters</button>')
       .insertBefore($('table'));
 
-    function matchesFilter(filterTextArray, value) {
-      return filterTextArray.reduce((isMatched, filter) => {
-        if (!isMatched) { return false; }
-        return filter.startsWith('^') ? !value.includes(filter.substr(1)) : value.includes(filter);
-      }, true);
-    }
-
-    function handleFilter() {
-      $('.item').hide();
-
-      const onlyAvailable = $('#showAvailableOnly').is(':checked');
-      const onlyUnsigned = $('#showUnsignedOnly').is(':checked');
-      let $rows = (onlyAvailable) ? $('.item:not(.historic-item)') : $('.item');
-
-      if (onlyUnsigned) {
-        $rows = $($rows.toArray().filter(item => $(item).find('.request-status').text().trim() === 'REQ'));
-      }
-
-      const itemFilterTextArray = $('#itemFilter').val().toLowerCase().trim()
-        .split(' ');
-      const notesFilterTextArray = $('#notesFilter').val().toLowerCase().trim()
-        .split(' ');
-      const countryFilterTextArray = $('#countryFilter').val().toLowerCase().trim()
-        .split(' ');
-      const priceFilter = $('#priceFilter').val();
-
-      $rows.each((index, row) => {
-        const $row = $(row);
-        const item = {
-          name: $row.find('.item-name').text().toLowerCase(),
-          notes: $row.find('.notes').text().toLowerCase(),
-          price: Number($($row.children()[2]).text().replace(/[^0-9.]+/g, '')),
-          country: $($row.children()[0]).text().toLowerCase(),
-        };
-        const rowMatched = matchesFilter(itemFilterTextArray, item.name) &&
-          matchesFilter(notesFilterTextArray, item.notes) &&
-          matchesFilter(countryFilterTextArray, item.country) &&
-          (priceFilter === '' || item.price <= priceFilter);
-
-        if (rowMatched) {
-          $row.show();
-        } else {
-          $row.hide();
-        }
-      });
-
-      $('#count').html($('.item:visible').length);
-    }
-
-    function clearAll() {
-      $('#itemFilter').val('');
-      $('#notesFilter').val('');
-      $('#countryFilter').val('');
-      $('#priceFilter').val('');
-      $('#itemFilter').keyup();
-    }
-
-    $('#clearAll').click(clearAll);
-    $('#showUnsignedOnly').change(handleFilter);
-    $('#notesFilter').keyup(handleFilter);
-    $('#countryFilter').keyup(handleFilter);
-    $('#priceFilter').keyup(handleFilter);
-    $('#itemFilter').keyup(handleFilter);
+    $('#clearFilters').click(Rummager.clearFilters);
+    $('#showUnsignedOnly').change(Rummager.handleFilter);
+    $('#notesFilter').keyup(Rummager.handleFilter);
+    $('#countryFilter').keyup(Rummager.handleFilter);
+    $('#priceFilter').keyup(Rummager.handleFilter);
+    $('#itemFilter').keyup(Rummager.handleFilter);
   }
 }
-overlayRummager();
+
+Rummager.overlayRummager();
